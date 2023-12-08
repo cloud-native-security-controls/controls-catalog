2886969080cb361ab6798f1cdf6eecb17986fa0e
AbiDabi123 <93230887+AbiDabi123@users.noreply.github.com>
2023-12-08 09:40:11 -0600

    Update csv_to_oscal.py
    Co-authored-by: JonZeolla <Zeolla@gmail.com>

--- csv_to_oscal.py
+++ csv_to_oscal.py
@@ -30,7 +30,7 @@ def read_csv(file_path: PathLike, *args, **kwargs) -> list[list[str]]:
             csv_reader = csv.reader(fd, *args, **kwargs)
             return list(csv_reader)
     except Exception as err:
-        logging.exception(f"error reading csv": {err}")
+        logging.exception(f"error reading csv: {err}")
         sys.exit(1)
 
 
